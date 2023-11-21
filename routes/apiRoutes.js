96b9beff880300fc5b4542b5ad6fd02c7444f745
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-11-21 01:36:32 -0600

    BUTTONS WORKING PROPERLY
    - Delete route was finalized. It disappears from the front end and is deleted in the backend. Save button works. Delete button works. Clear Form button works.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,8 +23,23 @@ router.post('/notes', (req, res) => {
   res.status(200).json({message: 'Note added successfully', note: newNote});
 });
 
-// router.delete('/noes', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
 
-// })
+  const data = fs.readFileSync(fileName, 'utf8');
+  let parsedData = JSON.parse(data);
+
+  const noteIndex = parsedData.findIndex((note) => note.id === noteId);
+
+  if (noteId !== -1) {
+    parsedData = parsedData.filter((note) => note.id !== noteId);
+
+    fs.writeFileSync(fileName, JSON.stringify(parsedData, null, 2));
+
+    res.status(200).json({ message: 'Note successfully deleted'});
+  } else {
+    res.status(404).json({ error: 'Note not found'});
+  }
+});
 
 module.exports = router;
